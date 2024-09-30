a1765f47c16051dde7c29c743eb095fe298b22df
ferdinand0815 <73479504+ferdinand0815@users.noreply.github.com>
2024-09-30 13:32:42 +0200

    Test compile ESP libs

--- examples/aaa/aaa.ino
+++ examples/aaa/aaa.ino
@@ -2,6 +2,18 @@
 
 #include <Arduino.h>
 
+
+#include <ArduinoMqttClient.h>
+#include <ESPAsyncWebServer.h>
+
+#ifdef ESP8266
+    #include <ESP8266WiFi.h>
+#else
+    #include <WiFi.h>
+    #include <IPAddress.h>
+#endif
+
+
 void setup() {
 };
 void loop() {
