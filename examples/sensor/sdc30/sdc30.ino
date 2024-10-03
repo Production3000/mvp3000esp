3332372c05167de936deadb8b774e233049b4d29
ferdinand0815 <73479504+ferdinand0815@users.noreply.github.com>
2024-10-03 17:20:22 +0200

    Fix SDC30 calibration, needs wait time afterwards

--- examples/sensor/sdc30/sdc30.ino
+++ examples/sensor/sdc30/sdc30.ino
@@ -23,6 +23,18 @@ extern MVP3000 mvp;
 #include <Wire.h>
 SensirionI2cScd30 sdc30;
 
+uint16_t athmosphericCO2 = 419; // ppm
+uint16_t siteAltitude = 450; // m
+uint16_t temperatureCompensation = 350; // °C*100 - The library function requests an unsigned value.
+
+enum OPERATINGSTATE: uint8_t {
+    ERROR = 0,
+    MEASURE = 1,
+    CALIBRATE = 2,
+} operatingState;
+uint8_t calibrationCounter = 0;
+
+
 const uint8_t valueCount = 3;
 
 // Add a description of the sensor for the web interface
@@ -60,13 +72,18 @@ void setup() {
 
     // Init SCD30 sensor
     sdc30.begin(Wire, SCD30_I2C_ADDR_61);
-    sdc30.stopPeriodicMeasurement();
+    // sdc30.stopPeriodicMeasurement();
     int16_t sdcError = sdc30.softReset(); // Blocking 2000 ms
     if (sdcError != NO_ERROR) {
         static char errorMessage[128];
-        errorToString(sdcError, errorMessage, sizeof errorMessage); // not sure if this works for the combined error codes
+        errorToString(sdcError, errorMessage, sizeof errorMessage);
         mvp.logFormatted("Error starting sensor: %s", errorMessage);
+        operatingState = OPERATINGSTATE::ERROR;
+        return;
     }
+
+    operatingState = OPERATINGSTATE::MEASURE;
+    sdc30.setTemperatureOffset(temperatureCompensation);
 
     // Get and print sensor information
     uint8_t major = 0;
@@ -76,59 +93,75 @@ void setup() {
     sdc30.getAltitudeCompensation(altitude);
     uint16_t temperatureOffset;
     sdc30.getTemperatureOffset(temperatureOffset);
-    uint16_t co2RefConcentration;
-    sdc30.getForceRecalibrationStatus(co2RefConcentration);
+    // uint16_t co2RefConcentration;
+    // sdc30.getForceRecalibrationStatus(co2RefConcentration); // This always returns 400 after softReset/power cycle. However, the sensor still uses the updated calibration curve.
     uint16_t isActive;
     sdc30.getAutoCalibrationStatus(isActive);
-    mvp.logFormatted("SDC30: Firmware version %d.%d, altitude %d m, temp offset %d C, sensor CO2 ambient %d ppm, auto calibration %s", major, minor, altitude, temperatureOffset, co2RefConcentration, (isActive) ? "active" : "disabled");
+    mvp.logFormatted("SDC30: Firmware version %d.%d, altitude %d m, temp offset %d C, auto calibration %s", major, minor, altitude, temperatureOffset, (isActive) ? "active" : "disabled");
 
     // Start periodic measurement
+    // This is ambiguous in the library: "Setting the ambient pressure will overwrite previous settings of altitude compensation. Setting the argument to zero will deactivate the ambient pressure compensation."
+    // Does this mean setting to zero will result in compensation for the current altitude? One can assume so, but just set the altitude again to make sure ...
     sdc30.startPeriodicMeasurement(0);
+    sdc30.setAltitudeCompensation(siteAltitude);
 }
 
 void loop() {
-    uint16_t sdcDataReady;
-    sdc30.getDataReady(sdcDataReady);
-    if (sdcDataReady) {
-        int16_t sdcError = sdc30.readMeasurementData(data[0], data[1], data[2]);
-        if (sdcError == NO_ERROR) {
-            // Add data to the sensor module
-            if (data[0] > 1) // First CO2 measurement is always 0, probably some running median thing
-                xmoduleSensor.addSample(data);
-        } else {
-            mvp.logFormatted("Error trying to execute readMeasurementData()");
-        }
-    }
-
     // Do the work
     mvp.loop();
-}
 
-
-boolean networkCtrlCallback(char* data) {
-    if (strcmp(data, "CALIBRATE") == 0) {
-        calibrateSensor();
-        return true;
-    }
-    return false;
-}
-
-void calibrateSensor() {
-    // Following the forced re-calibration (FRC) as described in the datasheet white paper.
-    mvp.log("The device needs to be OPERATING at FRESH air for AT LEAST 5 minutes before calibration!");
-
-    // Datasheet states 2 minutes. 
-    if (millis() < 5*60*1000) {
-        mvp.log("Calibration failed. Uptime less than 5 minutes.");
+    if (operatingState == OPERATINGSTATE::ERROR) {
         return;
     }
 
-    // Set calibration, this offsets essentially the current measurement
-    sdc30.forceRecalibration(420);
+    uint16_t sdcDataReady;
+    sdc30.getDataReady(sdcDataReady);
+    if (sdcDataReady) {
+        if (sdc30.readMeasurementData(data[0], data[1], data[2]) == NO_ERROR) {
+            if (operatingState == OPERATINGSTATE::MEASURE) {
+                // Add data to the sensor module
+                // The first CO2 measurement is always 0, probably some running median thing
+                if (data[0] > athmosphericCO2 - 100)
+                    xmoduleSensor.addSample(data);
+            }
+            if (operatingState == OPERATINGSTATE::CALIBRATE) {
+                // The sensor needs 1-3 measurement cycles to update the calibration curve
+                if (calibrationCounter++ > 2)
+                    operatingState = OPERATINGSTATE::MEASURE;
+            }
+        } else {
+            mvp.log("Error trying to execute readMeasurementData()");
+        }
+    }
+}
 
-    mvp.log("Calibration successfull. Restarting measurements");
-    // Restart sensor
-    sdc30.stopPeriodicMeasurement();
-    sdc30.softReset(); // Blocking 2000 ms
-    sdc30.startPeriodicMeasurement(0);
+
+void networkCtrlCallback(const String& data) {
+    if (data.equals("CALIBRATE")) {
+        calibrateSensor();
+    } else {
+        mvp.log("Unknown network control command.");
+    }
+}
+
+void calibrateSensor() {
+    // Datasheet states 2 minutes for the sensor, but without housing.
+    if (millis() < 5*60*1000) {
+        mvp.log("Calibration failed. The device needs to be OPERATING at FRESH air for AT LEAST 5 minutes before calibration!");
+        return;
+    }
+
+    // Set calibrating step
+    operatingState = OPERATINGSTATE::CALIBRATE;
+    calibrationCounter = 0;
+
+    int16_t sdcError = sdc30.forceRecalibration(athmosphericCO2); // Blocking 10 ms
+    if (sdcError != NO_ERROR) {
+        static char errorMessage[128];
+        errorToString(sdcError, errorMessage, sizeof errorMessage);
+        mvp.logFormatted("Calibration failed. Error during calibration: %s", errorMessage);
+        operatingState = OPERATINGSTATE::ERROR;
+    } else {
+        mvp.logFormatted("Calibration to %d ppm initiated. Restarting measurement ...", athmosphericCO2);
+    }
 }
